c6199fff2b2d73d4d162760d3f1e2d32336f2d2d
william serna <wserna880@gmail.com>
2022-09-12 20:25:08 -0500

    Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 
 app.use(express.static('public'));
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 app.get('/', (req, res) => {
